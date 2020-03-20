14864b227014ccfcf2db3aea92490a2b866748e1
Naufal Fachrezi <naufalf@Naufals-MacBook-Pro.local>
2020-03-20 14:39:38 +0700

    nopal

---
+++ Assets/ballmovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmovement : MonoBehaviour
+{
+
+    public int speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-1,-1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.name == "dindingKanan" || other.collider.name == "dindingKiri"){
+            GetComponent<Transform>().position = new Vector2(0,0);
+        }
+    }
+}
